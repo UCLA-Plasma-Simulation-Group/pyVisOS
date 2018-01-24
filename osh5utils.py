d56c52f10cf455721498dd664ad68f13e0c157ec
Han Wen <hanwen@ucla.edu>
2018-01-23 20:14:42 -0800

    add field decomposition function

--- osh5utils.py
+++ osh5utils.py
@@ -4,6 +4,7 @@ import osh5def
 import osaxis
 import numpy as np
 import copy
+import re
 from functools import wraps, partial
 
 
@@ -28,6 +29,8 @@ def metasl(func, mapping=(0, 0)):
         for tp in kl:
             if hasattr(args[tp[0]], 'meta2dict'):
                 saved.insert(0, (args[tp[0]].meta2dict(), tp[1]))
+        if not iter(saved):
+            raise ValueError('Illegal mapping parameters')
         # execute user function
         out = func(*args, **kwargs)
         # load saved meta data into specified positions
@@ -72,24 +75,26 @@ def stack(arr, axis=0, axesdata=None):
 
 
 # #----------------------------------- FFT Wrappers ----------------------------------------
-# sfunc: for shifting; ffunc: for calculating frequency; ftfunc: for fft the data
+# sfunc: for shifting; ffunc: for calculating frequency; ftfunc: for fft the data; uafunc: for updating axes
 #
-def __idle(a, **kwargs):
+def __idle(a, *args, **kwargs):
     return a
 
 
 def __try_update_axes(updfunc):
-    def update_axes(a, idx, sfunc=__idle, ffunc=__idle):
-        try:
-            axes = a.axes
-        except AttributeError:  # no axes found
+    def update_axes(a, idx, shape, sfunc=__idle, ffunc=__idle):
+        if not hasattr(a, 'axes'):  # no axes found
             return
         try:
             iter(idx)
         except TypeError:
-            idx = tuple(range(len(axes))) if idx is None else (idx,)
+            idx = tuple(range(len(a.axes))) if idx is None else (idx,)
+        try:
+            iter(shape)
+        except TypeError:
+            shape = (shape,)
         # The data size can change due to the s (or n) keyword. We have to force axes update somehow.
-        updfunc(axes, idx, a.shape, sfunc=sfunc, ffunc=ffunc)
+        updfunc(a.axes, idx, shape, sfunc=sfunc, ffunc=ffunc)
     return update_axes
 
 
@@ -97,6 +102,7 @@ def __try_update_axes(updfunc):
 def _update_fft_axes(axes, idx, shape, sfunc, ffunc):
     key, en = ['NAME', 'LONG_NAME', 'UNITS'], ['K(', 'K(', '1/(']
     for i in idx:
+        axes[i].attrs.setdefault('shift', axes[i].min())  # save lower bound. value of axes
         axes[i].ax = sfunc(ffunc(shape[i], d=axes[i].increment())) * 2 * np.pi
         for k, e in zip(key, en):
             try:
@@ -106,10 +112,10 @@ def _update_fft_axes(axes, idx, shape, sfunc, ffunc):
 
 
 @__try_update_axes
-def _update_ifft_axes(axes, idx, shape, **unused):
+def _update_ifft_axes(axes, idx,  shape, sfunc, ffunc):
     key, en = ['NAME', 'LONG_NAME', 'UNITS'], [2, 2, 3]
     for i in idx:
-        axes[i].ax = np.arange(0, 2*np.pi/axes[i].increment(), shape[i])
+        axes[i].ax = ffunc(shape[i], d=axes[i].increment(), min=axes[i].attrs.get('shift', 0))
         for k, e in zip(key, en):
             try:
                 axes[i].attrs[k] = axes[i].attrs[k][e:-1]
@@ -117,142 +123,236 @@ def _update_ifft_axes(axes, idx, shape, **unused):
                 pass
 
 
-def __fft_interface(ftfunc, sfunc):
+def _get_ihfft_axis(n, d=1.0, min=0.0):
+    length = 2 * np.pi / d
+    return np.arange(min, length + min, length / n)[0: n//2+1]
+
+
+def _get_ifft_axis(n, d=1.0, min=0.0):
+    length = 2 * np.pi / d
+    return np.arange(min, length + min, length / n)
+
+
+def __ft_interface(ftfunc, sfunc):
     @metasl
-    def fft_interface(a, s, axes, norm):
+    def ft_interface(a, s, axes, norm):
         # call fft and shift the result
-        return sfunc(ftfunc(a, s=s, axes=axes, norm=norm), axes=axes)
-    return fft_interface
+        return sfunc(ftfunc(sfunc(a, axes), s, axes, norm), axes)
+    return ft_interface
 
 
-def __shifted_fft_gen(ftfunc, sfunc, ffunc):
+def __shifted_ft_gen(ftfunc, sfunc, ffunc, uafunc):
     def shifted_fft(a, s=None, axes=None, norm=None):
-        o = __fft_interface(ftfunc, sfunc=sfunc)(a, s=s, axes=axes, norm=norm)
-        _update_fft_axes(o, axes, sfunc=sfunc, ffunc=ffunc)
+        shape = s if s is not None else a.shape
+        o = __ft_interface(ftfunc, sfunc=sfunc)(a, s=s, axes=axes, norm=norm)
+        uafunc(o, axes, shape, sfunc=sfunc, ffunc=ffunc)
         return o
     return shifted_fft
 
 
-def __ifft_interface(iftfunc, sfunc):
-    @metasl
-    def ifft_interface(a, s, axes, norm):
-        # call ifft and shift the result
-        return iftfunc(sfunc(a, axes=axes), s=s, axes=axes, norm=norm)
-    return ifft_interface
-
-
-def __shifted_ifft_gen(iftfunc, sfunc):
-    def shifted_ifft(a, s=None, axes=None, norm=None):
-        o = __ifft_interface(iftfunc, sfunc=sfunc)(a, s=s, axes=axes, norm=norm)
-        _update_ifft_axes(o, axes)
-        return o
-    return shifted_ifft
-
-
 # # ========  Normal FFT  ==========
-__shifted_fft = partial(__shifted_fft_gen, sfunc=np.fft.fftshift, ffunc=np.fft.fftfreq)
-__shifted_ifft = partial(__shifted_ifft_gen, sfunc=np.fft.ifftshift)
+__shifted_fft = partial(__shifted_ft_gen, sfunc=np.fft.fftshift, ffunc=np.fft.fftfreq, uafunc=_update_fft_axes)
+__shifted_ifft = partial(__shifted_ft_gen, sfunc=np.fft.ifftshift, ffunc=_get_ifft_axis, uafunc=_update_ifft_axes)
 
-@__shifted_fft
+
 def fftn(a, s=None, axes=None, norm=None):
-    return np.fft.fftn(a, s=s, axes=axes, norm=norm)
+    return __shifted_fft(np.fft.fftn)(a, s=s, axes=axes, norm=norm)
 
 
-@__shifted_fft
-def fft2(a, s=None, axes=None, norm=None):
-    return np.fft.fft2(a, s=s, axes=axes, norm=norm)
+def fft2(a, s=None, axes=(-2, -1), norm=None):
+    return __shifted_fft(np.fft.fft2)(a, s=s, axes=axes, norm=norm)
 
 
-@__shifted_fft
-def fft(a, s=None, axes=None, norm=None):
-    if axes is None:
-        axes = -1
-    return np.fft.fft(a, n=s, axis=axes, norm=norm)
+def fft(a, n=None, axis=-1, norm=None):
+    return __shifted_fft(np.fft.fft)(a, s=n, axes=axis, norm=norm)
 
 
-@__shifted_ifft
 def ifftn(a, s=None, axes=None, norm=None):
-    return np.fft.ifftn(a, s=s, axes=axes, norm=norm)
+    return __shifted_ifft(np.fft.ifftn)(a, s=s, axes=axes, norm=norm)
 
 
-@__shifted_ifft
-def ifft2(a, s=None, axes=None, norm=None):
-    return np.fft.ifft2(a, s=s, axes=axes, norm=norm)
+def ifft2(a, s=None, axes=(-2, -1), norm=None):
+    return __shifted_ifft(np.fft.ifft2)(a, s=s, axes=axes, norm=norm)
 
 
-@__shifted_ifft
-def ifft(a, s=None, axes=None, norm=None):
-    if axes is None:
-        axes = -1
-    return np.fft.ifft(a, n=s, axis=axes, norm=norm)
+def ifft(a, n=None, axis=-1, norm=None):
+    # if axes is None:
+    #     axes = -1
+    return __shifted_ifft(np.fft.ifft)(a, s=n, axes=axis, norm=norm)
 
 
 # # ========  real FFT  ==========
-__shifted_rfft = partial(__shifted_fft_gen, sfunc=__idle, ffunc=np.fft.rfftfreq)
-__shifted_irfft = partial(__shifted_ifft_gen, sfunc=__idle)
-
-@__shifted_rfft
-def rfftn(a, s, axes, norm):
-    return np.fft.rfftn(a, s=s, axes=axes, norm=norm)
+__shifted_rfft = partial(__shifted_ft_gen, sfunc=__idle, ffunc=np.fft.rfftfreq, uafunc=_update_fft_axes)
+__shifted_irfft = partial(__shifted_ft_gen, sfunc=__idle, ffunc=_get_ifft_axis, uafunc=_update_ifft_axes)
 
 
-@__shifted_rfft
-def rfft2(a, s, axes, norm):
-    return np.fft.rfft2(a, s=s, axes=axes, norm=norm)
+def __save_space_shape(a, s):
+    if isinstance(a, osh5def.H5Data):
+        shape = s if s is not None else a.shape
+        a.data_attrs.setdefault('oshape', shape)
 
 
-@__shifted_rfft
-def rfft(a, s, axes, norm):
-    if axes is None:
-        axes = -1
-    return np.fft.rfft(a, n=s, axis=axes, norm=norm)
+def __restore_space_shape(xdfunc):
+    def get_shape(a, s, axes):
+        if s is not None:
+            return s
+        if isinstance(a, osh5def.H5Data):
+            return xdfunc(a, s, axes)
+    return get_shape
 
 
-@__shifted_irfft
-def rfftn(a, s, axes, norm):
-    return np.fft.rfftn(a, s=s, axes=axes, norm=norm)
+@__restore_space_shape
+def __rss_1d(a, s, axes):
+    return a.data_attrs['oshape'][-1] if axes is None else a.data_attrs['oshape'][axes]
 
 
-@__shifted_irfft
-def rfft2(a, s, axes, norm):
-    return np.fft.rfft2(a, s=s, axes=axes, norm=norm)
+@__restore_space_shape
+def __rss_2d(a, s, axes):
+    return a.data_attrs['oshape'][-2:] if axes is None else tuple([a.data_attrs['oshape'][i] for i in axes])
 
 
-@__shifted_irfft
-def rfft(a, s, axes, norm):
-    if axes is None:
-        axes = -1
-    return np.fft.rfft(a, n=s, axis=axes, norm=norm)
+@__restore_space_shape
+def __rss_nd(a, s, axes):
+    return a.data_attrs['oshape'] if axes is None else tuple([a.data_attrs['oshape'][i] for i in axes])
+
+
+def rfftn(a, s=None, axes=None, norm=None):
+    __save_space_shape(a, s)
+    return __shifted_rfft(np.fft.rfftn)(a, s=s, axes=axes, norm=norm)
+
+
+def rfft2(a, s=None, axes=(-2, -1), norm=None):
+    __save_space_shape(a, s)
+    return __shifted_rfft(np.fft.rfft2)(a, s=s, axes=axes, norm=norm)
+
+
+def rfft(a, n=None, axis=-1, norm=None):
+    __save_space_shape(a, n)
+    return __shifted_rfft(np.fft.rfft)(a, s=n, axes=axis, norm=norm)
+
+
+def irfftn(a, s=None, axes=None, norm=None):
+    s = __rss_nd(a, s, axes)
+    return __shifted_irfft(np.fft.irfftn)(a, s=s, axes=axes, norm=norm)
+
+
+def irfft2(a, s=None, axes=(-2, -1), norm=None):
+    s = __rss_2d(a, s, axes)
+    return __shifted_irfft(np.fft.irfft2)(a, s=s, axes=axes, norm=norm)
+
+
+def irfft(a, n=None, axis=-1, norm=None):
+    n = __rss_1d(a, n, axis)
+    return __shifted_irfft(np.fft.irfft)(a, s=n, axes=axis, norm=norm)
 
 
 # # ========  Hermitian FFT  ==========
-@__shifted_irfft  # the axes operation is the same as real FFT
-def hfft(a, s, axes, norm):
-    if axes is None:
-        axes = -1
-    return np.fft.hfft(a, n=s, axis=axes, norm=norm)
+__shifted_hfft = partial(__shifted_ft_gen, sfunc=np.fft.fftshift, ffunc=np.fft.fftfreq, uafunc=_update_fft_axes)
+__shifted_ihfft = partial(__shifted_ft_gen, sfunc=__idle, ffunc=_get_ihfft_axis, uafunc=_update_ifft_axes)
 
 
-@__shifted_irfft
-def ihfft(a, s, axes, norm):
-    if axes is None:
-        axes = -1
-    return np.fft.ihfft(a, n=s, axis=axes, norm=norm)
+def hfft(a, n=None, axis=-1, norm=None):
+    if n is None:
+        n = a.shape[-1] if axis is None else a.shape[axis]
+    nn = 2*n - 1 if n % 2 else 2*n - 2
+    return __shifted_hfft(np.fft.hfft)(a, s=nn, axes=axis, norm=norm)
+
+
+def ihfft(a, n=None, axis=-1, norm=None):
+    return __shifted_ihfft(np.fft.ihfft)(a, s=n, axes=axis, norm=norm)
 # ----------------------------------- FFT Wrappers ----------------------------------------
+
+
+def field_decompose(fldarr, ffted=True, idim=None, finalize=None, outquants=('L', 't')):
+    """decompose a vector field into transverse and longitudinal direction
+    fldarr: list of field components in the order of x, y, z
+    ffted: have the input field been
+    finalize: what function to call after all transforms,
+        for example finalize=abs will be converted the fields to amplitude
+    idim: inverse fourier transform in idim direction(s)
+    outquonts: output quantities: default=('L','t')
+        'L': total amplitude square of longitudinal components
+        'T': total amplitude square of transverse components
+        't' or 't1', 't2', ...: transverse components, 't' means all transverse components
+        'l' or 'l1', 'l2', ...: longitudinal components, 'l' means all longitudinal components
+    return: list of field components in the following order (if some are not requested they will be simply omitted):
+        ['L', 'T', 't', 'l']
+    """
+    dim = len(fldarr)
+    if dim != fldarr[0].ndim:
+        raise IndexError('Not enough field components for decomposition')
+    if fldarr[0].ndim == 1:
+        return copy.deepcopy(fldarr)
+    if not finalize:
+        finalize = __idle
+
+    def wrap_up(data):
+        if idim:
+            return fftn(data, axes=idim)
+        else:
+            return data
+
+    def rename(fld, name, longname):
+        if isinstance(fld, osh5def.H5Data):
+            # replace numbers in the string
+            fld.name = re.sub("\d+", fld.name, name)
+            fld.data_attrs['NAME'] = re.sub("\d+", name, fld.data_attrs.get('NAME', fld.name))
+            fld.data_attrs['LONG_NAME'] = re.sub("\d+", longname, fld.data_attrs.get('LONG_NAME', ''))
+        return fld
+
+    if ffted:
+        fftfld = [copy.deepcopy(fi) for fi in fldarr]
+    else:
+        fftfld = [fftn(fi) for fi in fldarr]
+    kv = np.meshgrid(*reversed([x.ax for x in fftfld[0].axes]), sparse=True)
+    k2 = np.sum(ki**2 for ki in kv)  # |k|^2
+    k2[k2 == 0.0] = float('inf')
+    kdotfld = np.divide(np.sum(np.multiply(ki, fi) for ki, fi in zip(kv, fftfld)), k2)
+    fL, fT, ft, fl = 0, 0, [], []
+    for i, fi in enumerate(fftfld):
+        tmp = kdotfld * kv[i]
+        if 't' in outquants or 't' + str(i + 1) in outquants:
+            ft.append((finalize(wrap_up(fftfld[i] - tmp)), '{t' + str(i + 1) + '}'))
+            if 'T' in outquants:
+                fT += np.abs(ft[-1][0])**2
+        elif 'T' in outquants:
+            fT += np.abs(wrap_up(fftfld[i] - tmp))**2
+
+        if 'l' in outquants or 'l'+str(i+1) in outquants:
+            fl.append((finalize(wrap_up(tmp)), '{l'+str(i+1) + '}'))
+            if 'L' in outquants:
+                fL += np.abs(fl[-1][0])**2
+        elif 'L' in outquants:
+            fL += np.abs(wrap_up(tmp))**2
+
+    res = []
+    if not isinstance(fL, int):
+        res.append(rename(fL, 'L', 'L^2'))
+    if not isinstance(fT, int):
+        res.append(rename(fT, 'T', 'T^2'))
+    if ft:
+        for fi in ft:
+            res.append(rename(fi[0], fi[1], fi[1]))
+    if fl:
+        for fi in fl:
+            res.append(rename(fi[0], fi[1], fi[1]))
+    return tuple(res)
 
 
 if __name__ == '__main__':
     import osh5io
-    fn = 'test-123456.h5'
+    fn = 'n0-123456.h5'
     d = osh5io.read_h5(fn)
     # d = np.ones((3,2))
+    print(repr(d))
     c = hfft(d)
+    print('c = ', repr(c))
     b = ihfft(c)
     print('b is d? ', b is d)
-    e = d - b
-    print('b - d = ', e.view(np.ndarray))
-    print(b.data_attrs['UNITS'])
+    diff = d - b
+    print('b - d = ', diff.view(np.ndarray))
+    print(repr(b))
     b = np.sqrt(b)
-    print(b.data_attrs['UNITS'])
+    # print(repr(b))
 
 
