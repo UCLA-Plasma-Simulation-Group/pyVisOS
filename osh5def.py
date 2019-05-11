UCLA-Plasma-Simulation-Group/pyVisOS

commit 679a11a94deebf2b3890845ff6231fcbce456246

optionally change default units in plots

osh5def.py
@@ -139,7 +139,8 @@ class DataAxis:
 
 
 class OSUnits:
-    name = ['m_e', 'c', '\omega_p', 'e', 'n_0']
+    name = ('m_e', 'c', '\omega_p', 'e', 'n_0')
+    disp_name = ['m_e', 'c', '\omega_p', 'e', 'n_0', 'a.u.']
     xtrnum = re.compile(r"(?<=\^)\d+|(?<=\^{).*?(?=})")
 
     def __init__(self, s):
@@ -174,7 +175,7 @@ class OSUnits:
                         raise KeyError('Unknown unit: ' + re.findall(r'\w+', ss)[0])
 
     def tex(self):
-        return '$' + self.__str__() + '$'
+        return '$' + self.__str__() + '$' if self.__str__() else self.__str__()
 
     def limit_denominator(self, max_denominator=64):
         """call fractions.Fraction.limit_denominator method for each base unit"""
@@ -218,14 +219,14 @@ class OSUnits:
 
     def __str__(self):
         disp = ''.join(['' if p == 0 else n + " " if p == 1 else n + '^{' + str(p) + '} '
-                        for n, p in zip(OSUnits.name, self.power)])
+                        for n, p in zip(OSUnits.disp_name[:-1], self.power)])
         if not disp:
-            return 'a.u.'
+            return OSUnits.disp_name[-1]
         return disp
 
     def __repr__(self):
         return ''.join([str(self.__class__.__module__), '.', str(self.__class__.__name__), ' at ', hex(id(self)),
-                        ': ', repr(self.name), '=[', ', '.join([str(fr) for fr in self.power]), ']'])
+                        ': ', repr(self.name), '=(', ', '.join([str(fr) for fr in self.power]), ')'])
 
 
 class _LocIndexer(object):

osh5io.py
@@ -91,6 +91,7 @@ def read_h5(filename, path=None, axis_name="AXIS/AXIS"):
                 run_attrs[key] = value[0].decode('utf-8') if isinstance(value[0], bytes) else value
             except IndexError:
                 run_attrs[key] = value.decode('utf-8') if isinstance(value, bytes) else value
+        run_attrs['TIME UNITS'] = OSUnits(run_attrs['TIME UNITS'])
         # attach attributes assigned to the data array to
         #    the H5Data.data_attrs object, remove trivial dimension before assignment
         for key, value in the_data_hdf_object.attrs.items():
@@ -159,7 +160,7 @@ def read_h5_openpmd(filename, path=None):
             meshPath = basePath + data_file.attrs['meshesPath'].decode('utf-8')
             run_attrs = {k.upper(): v for k, v in
                          data_file[basePath].attrs.items()}
-        run_attrs.setdefault('TIME UNITS', r'1 / \omega_p')
+        run_attrs.setdefault('TIME UNITS', OSUnits('1 / \omega_p'))
 
         # read field data
         lname_dict, fldl = {'E1': 'E_x', 'E2': 'E_y', 'E3': 'E_z',
@@ -293,7 +294,7 @@ def write_h5(data, filename=None, path=None, dataset_name=None, overwrite=True, 
     h5file.attrs['MOVE C'] = [0]
     h5file.attrs['PERIODIC'] = [0]
     h5file.attrs['TIME'] = [0.0]
-    h5file.attrs['TIME UNITS'] = [b'']
+    h5file.attrs['TIME UNITS'] = [b'1 / \omega_p']
     h5file.attrs['TYPE'] = [b'grid']
     h5file.attrs['XMIN'] = [0.0]
     h5file.attrs['XMAX'] = [0.0]
@@ -396,7 +397,7 @@ def write_h5_openpmd(data, filename=None, path=None, dataset_name=None, overwrit
     h5file.attrs['MOVE C'] = [0]
     h5file.attrs['PERIODIC'] = [0]
     h5file.attrs['TIME'] = [0.0]
-    h5file.attrs['TIME UNITS'] = [b'']
+    h5file.attrs['TIME UNITS'] = [b'1 / \omega_p']
     h5file.attrs['TYPE'] = [b'grid']
     h5file.attrs['XMIN'] = [0.0]
     h5file.attrs['XMAX'] = [0.0]

osh5vis.py
@@ -2,11 +2,21 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import numpy as np
+import osh5def
 # try:
 #     import osh5gui
 #     gui_fname = osh5gui.gui_fname
 # except ImportError:
 #     print('Fail to import GUI routines. Check your PyQT installation')
+
+
+def change_default_units(units):
+    osh5def.OSUnits.disp_name[0:len(units)] = units
+
+
+def default_units():
+    return osh5def.OSUnits.disp_name
+
 
 def time_format(time=0.0, unit=None, convert_tunit=False, wavelength=0.351, **kwargs):
     if convert_tunit:

commit ed855fb403abc0031bf3a005bfc2462db8be8cf9

fix a bug introduced by 679a11a in write_h5/read_h5

osh5def.py
@@ -135,11 +135,11 @@ class DataAxis:
             return wavelength / (2 * np.pi), '\mu m'
         if self.attrs['UNITS'].is_density():
             return density, 'cm^{-3}'
-        return 1.0, self.units
+        return 1.0, str(self.units)
 
 
 class OSUnits:
-    name = ('m_e', 'c', '\omega_p', 'e', 'n_0')
+    name = ('m_e', 'c', '\omega', 'e', 'n_0')
     disp_name = ['m_e', 'c', '\omega_p', 'e', 'n_0', 'a.u.']
     xtrnum = re.compile(r"(?<=\^)\d+|(?<=\^{).*?(?=})")
 

osh5io.py
@@ -106,7 +106,7 @@ def read_h5(filename, path=None, axis_name="AXIS/AXIS"):
         # convert unit string to osunit object
         try:
             data_attrs['UNITS'] = OSUnits(data_attrs['UNITS'])
-        except KeyError:
+        except:
             data_attrs['UNITS'] = OSUnits('a.u.')
         data_attrs['NAME'] = name
 